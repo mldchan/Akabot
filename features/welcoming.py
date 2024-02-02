5de260d9bafedc1d1afdd0738b8c17b84040d00b
mldkyt <mldkyt@proton.me>
2024-02-02 12:19:04 +0100

    Heh it pushed into main, vscode is showing python-rewrite

--- features/welcoming.py
+++ features/welcoming.py
@@ -1,6 +1,7 @@
 
 import discord
 import pymongo
+from utils.settings import get_setting
 
 class Welcoming(discord.Cog):
     def __init__(self, bot: discord.Bot, db: pymongo.MongoClient) -> None:
@@ -8,9 +9,28 @@ class Welcoming(discord.Cog):
         self.db = db
     
     @discord.Cog.listener()
-    async def on_member_join(member: discord.Member):
-        pass
+    async def on_member_join(self, member: discord.Member):
+        target_channel_id = get_setting(self.db, member.guild.id, 'welcome_channel', '0')
+        if target_channel_id == '0':
+            return # The channel was not set and was initialised as we just ran this
+        message_type = get_setting(self.db, member.guild.id, 'welcome_type', 'embed') # embed or text
+        message_title = get_setting(self.db, member.guild.id, 'welcome_title', 'Welcome') # embed title (if type is embed)
+        message_text = get_setting(self.db, member.guild.id, 'welcome_text', 'Welcome {user} to {server}!') # text of the message or description of embed
+
+        target_channel = member.guild.get_channel(target_channel_id)
+        if target_channel is None:
+            return # Cannot find channel, don't do any further actions
+        
+        message_title = message_title.replace('{user}', member.display_name)
+        message_title = message_title.replace('{server}', member.guild.name)
+        
+        message_text = message_text.replace('{user}', member.display_name)
+        message_text = message_text.replace('{server}', member.guild.name)
+
+        if message_type == 'embed':
+            embed = discord.Embed(title=message_title, description=message_text) # Create the embed
+            await target_channel.send(embed=embed) # Send it in the welcoming channel
 
     @discord.Cog.listener()
-    async def on_member_remove(member: discord.Member):
+    async def on_member_remove(self, member: discord.Member):
         pass
