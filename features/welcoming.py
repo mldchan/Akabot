05ef1ab68c2badc3c4e197eaaa44892a14b897e0
mldkyt <mldkyt@proton.me>
2024-02-02 13:30:50 +0100

    Add goodbye :3

--- features/welcoming.py
+++ features/welcoming.py
@@ -32,8 +32,33 @@ class Welcoming(discord.Cog):
         if message_type == 'embed':
             embed = discord.Embed(title=message_title, description=message_text) # Create the embed
             await target_channel.send(embed=embed) # Send it in the welcoming channel
+        if message_type == 'text':
+            await target_channel.send(content=message_text) # Send it as text only
 
     @discord.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
-        # TODO: Write this (duh)
-        pass
+        target_channel_id = get_setting(self.db, member.guild.id, 'goodbye_channel', '0')
+        if target_channel_id == '0':
+            return # The channel was not set and was initialised as we just ran this
+        message_type = get_setting(self.db, member.guild.id, 'goodbye_type', 'embed') # embed or text
+        message_title = get_setting(self.db, member.guild.id, 'goodbye_title', 'Goodbye') # embed title (if type is embed)
+        message_text = get_setting(self.db, member.guild.id, 'goodbye_text', 'Goodbye {user}!') # text of the message or description of embed
+
+        target_channel = member.guild.get_channel(target_channel_id)
+        if target_channel is None:
+            return # Cannot find channel, don't do any further actions
+        
+        message_title = message_title.replace('{user}', member.display_name)
+        message_title = message_title.replace('{server}', member.guild.name)
+        message_title = message_title.replace('{memberCount}', member.guild.member_count)
+        
+        message_text = message_text.replace('{user}', member.display_name)
+        message_text = message_text.replace('{server}', member.guild.name)
+        message_text = message_text.replace('{memberCount}', member.guild.member_count)
+
+        if message_type == 'embed':
+            embed = discord.Embed(title=message_title, description=message_text) # Create the embed
+            await target_channel.send(embed=embed) # Send it in the welcoming channel
+        if message_type == 'text':
+            await target_channel.send(content=message_text) # Send it as text only
+        
