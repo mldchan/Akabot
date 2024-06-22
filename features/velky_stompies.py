5c2416fc20bcc0bcb43e6615695c35fe969d4f51
Akatsuki <akatsuki@noreply.codeberg.org>
2024-06-22 08:08:44 +0200

    Fixed settings bruh

--- features/velky_stompies.py
+++ features/velky_stompies.py
@@ -2,20 +2,25 @@ import discord
 
 from utils.settings import get_setting, set_setting
 from utils.logging import log_into_logs
+from discord.ext import commands
+from utils.blocked import is_blocked
 
 class VelkyStompies(discord.Cog):
 
     @discord.slash_command(name="stompies", description="Stompies")
     async def velky_stompies(self, ctx: discord.ApplicationContext):
-        if not get_setting(ctx.guild.id, "stompies_enabled", "True") == "True":
+        if get_setting(ctx.guild.id, "stompies_enabled", "True") == "False":
             await ctx.respond("The command is disabled", ephemeral=True)
             return
 
         await ctx.respond("https://tenor.com/view/stompies-velky-cute-angwy-gif-13012534518393437613")
 
-    stompies_settings_group = discord.SlashCommandGroup("stompies", "Stompies commands")
+    stompies_settings_group = discord.SlashCommandGroup("stompies_settings", "Stompies commands")
 
     @stompies_settings_group.command(name="enable", description="Set enabled state")
+    @discord.option(name="enabled", description="Whether the stompies are enabled", type=bool)
+    @discord.default_permissions(manage_guild=True)
+    @is_blocked()
     async def stompies_enable(self, ctx: discord.ApplicationContext, enabled: bool):
         old_value = get_setting(ctx.guild.id, "stompies_enabled", str(enabled)) == "True"
 
@@ -24,7 +29,7 @@ class VelkyStompies(discord.Cog):
             logging_embed.add_field(name="User", value=f"{ctx.user.mention}")
             logging_embed.add_field(name="Value", value=f'{"Enabled" if old_value else "Disabled"} -> {"Enabled" if enabled else "Disabled"}')
 
-            await log_into_logs(ctx.guild.id, logging_embed)
+            await log_into_logs(ctx.guild, logging_embed)
 
         set_setting(ctx.guild.id, 'stompies_enabled', str(enabled))
 
