9fe22dc16a1168f5484bc793899a24b445b01edb
Akatsuki2555 <Akatsuki2555@proton.me>
2024-06-29 17:29:57 +0200

    idk how this happened

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from discord.ext import commands as discord_commands_ext
 
 from features import welcoming, leveling, antiraid, chat_streaks, chat_revive, chat_summary, reaction_roles, \
     feedback_cmd, logging_mod, admin_cmds, giveaways, feedback_cmd, moderation, cleanup_task, verification, velky_stompies, \
-    roles_on_join, auto_react, auto_response
+    roles_on_join
 from utils.blocked import BlockedUserError, BlockedServerError
 
 logger = logging.getLogger("Akatsuki")
@@ -89,7 +89,5 @@ bot.add_cog(moderation.Moderation(bot))
 bot.add_cog(verification.Verification(bot))
 bot.add_cog(velky_stompies.VelkyStompies())
 bot.add_cog(roles_on_join.RolesOnJoin(bot))
-bot.add_cog(auto_react.AutoReact(bot))
-bot.add_cog(auto_response.AutoResponse(bot))
 
 bot.run(data['token'])
