3bc1d0256ad335869ed07bef492ac5a9035d9ae9
Akatsuki2555 <Akatsuki2555@proton.me>
2024-07-08 20:19:37 +0200

    New example file

--- convert_config.py
+++ convert_config.py
@@ -3,57 +3,6 @@ import json
 print("Akabot Config Converter from 3.1 -> 3.2")
 print("This script will convert your config.json to config.conf format.")
 
-# New config
-# Admin_AdminGuildID=
-# Admin_OwnerID=
-# Bot_Version=
-# Sentry_Enabled=
-# Sentry_DSN=
-# Heartbeat_Enabled=
-# Bot_Token=
-# Feature_DatabaseCleanupTask=
-# Feature_WelcomeGoodbye=
-# Feature_Leveling=
-# Feature_AntiRaid=
-# Feature_ChatStreaks=
-# Feature_ChatRevive=
-# Feature_ChatSummary=
-# Feature_ReactionRoles=
-# Feature_Logging=
-# Feature_AdminCommands=
-# Feature_Giveaways=
-# Feature_FeedbackCmd=
-# Feature_Moderation=
-# Feature_Verification=
-# Feature_VelkyStompies=
-# Feature_RolesOnJoin=
-# Feature_Heartbeat=
-# Feature_AutomodActions=
-# GitHub_User=
-# GitHub_Repo=
-# GitHub_Token=
-# Old config
-# {
-#     "token": "token",
-#     "owner_id": 0,
-#     "admin_guild": 0,
-#     "github": {
-#         "token": "",
-#         "git_repo": "",
-#         "git_user": ""
-#     },
-#     "sentry": {
-#         "enabled": false,
-#         "dsn": ""
-#     },
-#     "heartbeat": {
-#         "enabled": false,
-#         "url": "",
-#         "method": "post",
-#         "interval": 30
-#     }
-# }
-
 with open("config.json", "r") as f:
     data = json.load(f)
 
@@ -69,7 +18,7 @@ Bot_Token={token}
 GitHub_User={github_user}
 GitHub_Repo={github_repo}
 GitHub_Token={github_token}
-            """.format(
+""".format(
                 admin_guild=data.get("admin_guild", "0"),
                 owner_id=data.get("owner_id", "0"),
                 sentry_enabled=data.get("sentry", {}).get("enabled", "false"),
