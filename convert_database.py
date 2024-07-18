7a6b744eda73852b7c3cec4e0791594284a49a22
Akatsuki2555 <Akatsuki2555@proton.me>
2024-07-18 20:42:04 +0200

    Update convert_database.py

--- convert_database.py
+++ convert_database.py
@@ -35,4 +35,26 @@ if "user_id" in column_names:
         "drop table giveaway_participants_old"
     )
 
+# Auto React table check if the name of the second column is "channel_id", if yes, drop the table
+print("Checking if auto_react table exists")
+cur.execute(
+    "PRAGMA table_info(auto_react)"
+)
+columns = cur.fetchall()
+column_names = [column[1] for column in columns]
+if "channel_id" in column_names:
+    print("Dropping auto_react table")
+    cur.execute("DROP TABLE auto_react")
+
+# Auto Response table check if the name of the second column is "channel_id", if yes, drop the table
+print("Checking if auto_response table exists")
+cur.execute(
+    "PRAGMA table_info(auto_response)"
+)
+columns = cur.fetchall()
+column_names = [column[1] for column in columns]
+if "channel_id" in column_names:
+    print("Dropping auto_response table")
+    cur.execute("DROP TABLE auto_response")
+
 conn.commit()
