1c657b657541af7ae2c41aef8e6f36a8b95fce48
Akatsuki2555 <Akatsuki2555@proton.me>
2024-08-01 21:42:51 +0200

    Revert 3 commits
    This reverts commit 50b5432a6c293224e2e8b3acb474ef69f18b61f0.

--- features/heartbeat.py
+++ features/heartbeat.py
@@ -3,7 +3,6 @@ import requests
 import logging
 
 from utils.config import get_key
-from discord.ext import tasks
 
 
 class Heartbeat(discord.Cog):
@@ -17,7 +16,7 @@ class Heartbeat(discord.Cog):
         else:
             logging.warning("Heartbeat is disabled")
 
-    @tasks.loop(seconds=1)
+    @discord.ext.tasks.loop(seconds=1)
     async def heartbeat_task(self):
         self.interval_cnt += 1
         if self.interval_cnt >= int(get_key("Heartbeat_Interval", '60')):
