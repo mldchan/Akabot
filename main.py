621694c910c7a1cc3fe05af4cb79a4af14fed478
Akatsuki2555 <akatsuki2555@proton.me>
2024-08-26 14:12:07 +0200

    Temporary voice channels (#46)

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from features import welcoming, leveling, antiraid, chat_streaks, chat_revive, c
     logging_mod, admin_cmds, giveaways, feedback_cmd, moderation, cleanup_task, verification, velky_stompies, \
     roles_on_join, heartbeat, automod_actions, power_outage_announcement, per_user_settings, server_settings, \
     auto_react, auto_response, bot_help, announcement_channels, tickets, debug_commands, birthday_announcements, \
-    send_server_count, suggestions
+    send_server_count, suggestions, temporary_vc
 from utils.config import get_key
 from utils.languages import get_translation_for_key_localized as trl
 
@@ -134,5 +134,7 @@ if get_key("Feature_BirthdayAnnouncements", "true") == "true":
 bot.add_cog(send_server_count.SendServerCount(bot))
 if get_key("Feature_SuggestionsEnabled", "true") == "true":
     bot.add_cog(suggestions.Suggestions(bot))
+if get_key('Feature_TemporaryVC', 'true') == 'true':
+    bot.add_cog(temporary_vc.TemporaryVC(bot))
 
 bot.run(get_key("Bot_Token"))
