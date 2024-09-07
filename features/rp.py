74cf1b0b91e6b9d55349debe85457fb95d86a5eb
mldkyt <mldkyt@proton.me>
2024-09-07 21:49:57 +0200

    Settings, analytics for RP commands

--- features/rp.py
+++ features/rp.py
@@ -2,7 +2,9 @@
 import json
 import random
 import discord
-
+from utils.settings import get_setting, set_setting
+from discord.ext import commands
+from utils.analytics import analytics
 
 def pick_hug_gif():
     with open("configs/rp.json", "r") as f:
@@ -43,36 +45,75 @@ class RoleplayCommands(discord.Cog):
     def __init__(self, bot):
         self.bot = bot
         
+    rp_admin_group = discord.SlashCommandGroup("rp_admin", "Admin roleplay commands")
     rp_group = discord.SlashCommandGroup("rp", "Roleplay commands")
     
     @rp_group.command(name="hug")
+    @analytics("rp hug")
     async def hug(self, ctx, member: discord.Member):
         """Hug a member"""
+        if get_setting(ctx.guild.id, "roleplay_enabled", "true") == "false":
+            await ctx.respond(content="Roleplay commands are disabled for this server", ephemeral=True)
+            return
+        
         gif = pick_hug_gif()
         embed = discord.Embed(title=f"{ctx.author.display_name} hugs {member.display_name}!", footer=discord.EmbedFooter(text=get_footer_msg()))
         embed.set_image(url=gif)
         await ctx.respond(content=f"{ctx.author.mention} hugs {member.mention}!", embed=embed)
 
     @rp_group.command(name="kiss")
+    @analytics("rp kiss")
     async def kiss(self, ctx, member: discord.Member):
         """Kiss a member"""
+        if get_setting(ctx.guild.id, "roleplay_enabled", "true") == "false":
+            await ctx.respond(content="Roleplay commands are disabled for this server", ephemeral=True)
+            return
+        
         gif = pick_kiss_yaoi_gif()
         embed = discord.Embed(title=f"{ctx.author.display_name} kisses {member.display_name}!", footer=discord.EmbedFooter(text=get_footer_msg()))
         embed.set_image(url=gif)
         await ctx.respond(content=f"{ctx.author.mention} kisses {member.mention}!", embed=embed)
 
     @rp_group.command(name="bite")
+    @analytics("rp bite")
     async def bite(self, ctx, member: discord.Member):
         """Bite a member"""
+        if get_setting(ctx.guild.id, "roleplay_enabled", "true") == "false":
+            await ctx.respond(content="Roleplay commands are disabled for this server", ephemeral=True)
+            return
+        
         gif = pick_bite_gif()
         embed = discord.Embed(title=f"{ctx.author.display_name} bites {member.display_name}!", footer=discord.EmbedFooter(text=get_footer_msg()))
         embed.set_image(url=gif)
         await ctx.respond(content=f"{ctx.author.mention} bites {member.mention}!", embed=embed)
         
     @rp_group.command(name="unbite")
+    @analytics("rp unbite")
     async def unbite(self, ctx, member: discord.Member):
         """Unbite a member"""
+        if get_setting(ctx.guild.id, "roleplay_enabled", "true") == "false":
+            await ctx.respond(content="Roleplay commands are disabled for this server", ephemeral=True)
+            return
+        
         embed = discord.Embed(title=f"{ctx.author.display_name} unbites {member.display_name}!", footer=discord.EmbedFooter(text=get_footer_msg()))
         embed.set_image(url=get_unbite_img())
         await ctx.respond(content=f"{ctx.author.mention} unbites {member.mention}!", embed=embed)
 
+    @rp_admin_group.command(name="enabled")
+    @analytics("rp_admin enabled")
+    async def rp_enable(self, ctx, enable: bool):
+        """Enable or disable roleplay commands"""        
+        set_setting(ctx.guild.id, "roleplay_enabled", str(enable).lower())
+        await ctx.respond(content=f"Roleplay commands have been {'enabled' if enable else 'disabled'}", ephemeral=True)
+        
+    @rp_admin_group.command(name="list")
+    @analytics("rp_admin list")
+    @discord.default_permissions(manage_guild=True)
+    @commands.has_permissions(manage_guild=True)
+    @discord.guild_only()
+    async def rp_list(self, ctx):
+        """List all settings for roleplay"""
+        rp_enabled = get_setting(ctx.guild.id, "roleplay_enabled")
+        embed = discord.Embed(title="Roleplay settings")
+        embed.add_field(name="Enabled", value='Yes' if rp_enabled else 'No', inline=False)
+        await ctx.respond(embed=embed, ephemeral=True)
