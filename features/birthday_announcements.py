26c13d3b8109c6ac78c26ecedac3cfa3f382825f
mldchan <mldkyt@proton.me>
2024-10-04 21:10:30 +0200

    Converted more things into MongoDB

--- features/birthday_announcements.py
+++ features/birthday_announcements.py
@@ -1,14 +1,9 @@
-import datetime
-
 import discord
 from discord.ext import tasks, commands
 
 from utils.analytics import analytics
 from utils.languages import get_translation_for_key_localized as trl
-from utils.per_user_settings import search_settings_by_value, get_per_user_setting
-from utils.settings import get_setting
 from utils.settings import set_setting
-from utils.tzutil import get_now_for_server
 
 
 class BirthdayAnnouncements(discord.Cog):
@@ -40,51 +35,8 @@ class BirthdayAnnouncements(discord.Cog):
 
     async def handle_birthday_announcements(self):
 
-        date_string = datetime.datetime.now().strftime("%m-%d")
+    # Check for birthdays
 
-        for guild in self.bot.guilds:
-            birthday_announcements_channel = int(get_setting(guild.id, "birthday_announcements_channel", "0"))
-            if birthday_announcements_channel == 0:
-                continue
-
-            now = get_now_for_server(guild.id)
-            if now.hour != 12 or now.minute != 0:
-                continue
-
-            birthdays_this_day = search_settings_by_value(date_string)
-            for i in birthdays_this_day:
-                member = guild.get_member(i[0])
-                if member is None:
-                    continue
-
-                if get_per_user_setting(member.id, f'birthday_announcements_{str(guild.id)}', 'true') == 'true':
-                    message = get_setting(guild.id, "birthday_announcements_message",
-                                          "Today is {mention}'s birthday! 🎉 Say happy birthday! 🎂")
-                    message = message.replace("{mention}", member.mention)
-                    message = message.replace("{name}", member.display_name)
-                    message = message.replace("{guild}", guild.name)
-                    message = message.replace("{birthday}", i[1])
-
-                    if get_per_user_setting(member.id, 'birthday_reveal_age', 'false') == 'true':
-                        message = message.replace("{age}", str(now.year - int(i[1][:4])))
-                    else:
-                        message = message.replace("{age}", "*this user's age is hidden*")
-
-                    # Send the message
-                    channel = guild.get_channel(birthday_announcements_channel)
-                    await channel.send(message)
-
-                # Send DM if enabled
-                if get_per_user_setting(member.id, 'birthday_send_dm', 'false') == 'true':
-                    try:
-                        if get_per_user_setting(member.id, 'birthday_reveal_age', 'false'):
-                            await member.send(
-                                trl(member.id, 0, "birthday_dm_age").format(age=str(now.year - int(i[1][:4]))))
-                        else:
-                            await member.send(trl(member.id, 0, "birthday_dm"))
-
-                    except discord.Forbidden:
-                        pass
 
     @birthday_announcements_group.command(name="message", description="Customize the birthday announcement message")
     @discord.option(name="message", description="The message to send on birthdays")
--- utils/warning.py
+++ utils/warning.py
@@ -1,14 +1,15 @@
 import datetime
 
 import discord
+from bson import ObjectId
 
-from database import conn
+from database import client
 from utils.generic import get_date_time_str, pretty_time_delta
 from utils.languages import get_translation_for_key_localized as trl
 from utils.settings import get_setting
 
 
-async def add_warning(user: discord.Member, guild: discord.Guild, reason: str) -> int:
+async def add_warning(user: discord.Member, guild: discord.Guild, reason: str) -> ObjectId:
     id = db_add_warning(guild.id, user.id, reason)
 
     warning_should_dm = get_setting(guild.id, 'send_warning_message', 'true')
@@ -33,26 +34,26 @@ async def add_warning(user: discord.Member, guild: discord.Guild, reason: str) -
         return id
 
     for action in actions:
-        if len(warnings) == action[2]:  # only apply if the number of warnings matches, not if below
-            if action[1] == 'kick':
+        if len(warnings) == action['Warnings']:  # only apply if the number of warnings matches, not if below
+            if action['Action'] == 'kick':
                 # try dm user
                 try:
                     await user.send(
-                        trl(user.id, guild.id, "warn_actions_auto_kick_dm").format(name=guild.name, warnings=action[2]))
+                        trl(user.id, guild.id, "warn_actions_auto_kick_dm").format(name=guild.name, warnings=action['Warnings']))
                 except Exception:
                     pass
                 await user.kick(
-                    reason=trl(user.id, guild.id, "warn_actions_auto_kick_reason").format(warnings=action[2]))
-            elif action[1] == 'ban':
+                    reason=trl(user.id, guild.id, "warn_actions_auto_kick_reason").format(warnings=action['Warnings']))
+            elif action['Action'] == 'ban':
                 # try dm user
                 try:
                     await user.send(
-                        trl(user.id, guild.id, "warn_actions_auto_ban_dm").format(name=guild.name, warnings=action[2]))
+                        trl(user.id, guild.id, "warn_actions_auto_ban_dm").format(name=guild.name, warnings=action['Warnings']))
                 except Exception:
                     pass
-                await user.ban(reason=trl(user.id, guild.id, "warn_actions_auto_ban_reason").format(warnings=action[2]))
-            elif action[1].startswith('timeout'):
-                time = action[1].split(' ')[1]
+                await user.ban(reason=trl(user.id, guild.id, "warn_actions_auto_ban_reason").format(warnings=action['Warnings']))
+            elif action['Action'].startswith('timeout'):
+                time = action['Action'].split(' ')[1]
                 total_seconds = 0
                 if time == '12h':
                     total_seconds = 43200
@@ -67,7 +68,7 @@ async def add_warning(user: discord.Member, guild: discord.Guild, reason: str) -
                 try:
                     await user.send(
                         trl(user.id, guild.id, "warn_actions_auto_timeout_dm").format(name=guild.name,
-                                                                                      warnings=action[2],
+                                                                                      warnings=action['Warnings'],
                                                                                       time=pretty_time_delta(
                                                                                           total_seconds,
                                                                                           user_id=user.id,
@@ -77,55 +78,38 @@ async def add_warning(user: discord.Member, guild: discord.Guild, reason: str) -
 
                 await user.timeout_for(datetime.timedelta(seconds=total_seconds),
                                        reason=trl(user.id, guild.id, "warn_actions_auto_timeout_reason").format(
-                                           warnings=action[2]))
+                                           warnings=action['Warnings']))
 
     return id
 
 
-def db_add_warning(guild_id: int, user_id: int, reason: str) -> int:
-    cur = conn.cursor()
-    cur.execute('insert into warnings (guild_id, user_id, reason, timestamp) values (?, ?, ?, ?)',
-                (guild_id, user_id, reason, get_date_time_str(guild_id)))
-    warning_id = cur.lastrowid
-    cur.close()
-    conn.commit()
-    return warning_id
+def db_add_warning(guild_id: int, user_id: int, reason: str) -> ObjectId:
+    res = client['Warnings'].insert_one({'GuildID': guild_id, 'UserID': user_id, 'Reason': reason, 'Timestamp': get_date_time_str(guild_id)})
+    return res.inserted_id
 
 
-def db_get_warnings(guild_id: int, user_id: int):
-    cur = conn.cursor()
-    cur.execute('select id, reason, timestamp from warnings where guild_id = ? and user_id = ?', (guild_id, user_id))
-    warnings = cur.fetchall()
-    cur.close()
-    return warnings
+def db_get_warnings(guild_id: int, user_id: int) -> list[dict]:
+    res = client['Warnings'].find({'GuildID': guild_id, 'UserID': user_id}).to_list()
+    return res
 
 
-def db_remove_warning(guild_id: int, warning_id: int):
-    cur = conn.cursor()
-    cur.execute('delete from warnings where guild_id = ? and id = ?', (guild_id, warning_id))
-    cur.close()
-    conn.commit()
+def db_remove_warning(guild_id: int, warning_id: str):
+    if not ObjectId.is_valid(warning_id):
+        return
+
+    client['Warnings'].delete_one({'GuildID': guild_id, '_id': ObjectId(warning_id)})
 
 
 def db_add_warning_action(guild_id: int, action: str, warnings: int):
-    # Add an action to be taken on a user with a certain number of warnings
-    cur = conn.cursor()
-    cur.execute('insert into warnings_actions (guild_id, action, warnings) values (?, ?, ?)',
-                (guild_id, action, warnings))
-    cur.close()
-    conn.commit()
+    client['WarningActions'].insert_one({'GuildID': guild_id, 'Action': action, 'Warnings': warnings})
 
 
-def db_get_warning_actions(guild_id: int):
-    cur = conn.cursor()
-    cur.execute('select id, action, warnings from warnings_actions where guild_id = ?', (guild_id,))
-    actions = cur.fetchall()
-    cur.close()
-    return actions
+def db_get_warning_actions(guild_id: int) -> list[dict]:
+    res = client['WarningActions'].find({'GuildID': guild_id}).to_list()
+    return res
 
 
-def db_remove_warning_action(id: int):
-    cur = conn.cursor()
-    cur.execute('delete from warnings_actions where id = ?', (id,))
-    cur.close()
-    conn.commit()
+def db_remove_warning_action(id: str):
+    if not ObjectId.is_valid(id):
+        return
+    client['WarningActions'].delete_one({'_id': id})
