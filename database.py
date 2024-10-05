7b1cfdba0c11c32b413dea4c951e1782536310ec
mldchan <mldkyt@proton.me>
2024-10-05 20:08:33 +0200

    Ability to log in to DB

--- database.py
+++ database.py
@@ -1,3 +1,10 @@
 from pymongo import MongoClient
 
-client = MongoClient('AUTH HERE', 27017)['akabot']
+from utils.config import get_key
+
+name = get_key("DB_Username", "")
+password = get_key("DB_Password", "")
+host = get_key("DB_Host", "localhost")
+port = get_key("DB_Port", "27017")
+
+client = MongoClient(f'mongodb://{name}:{password}@{host}:{port}/', 27017)['akabot']
